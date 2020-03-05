5b18aa544f73fada230b604afa34978535599766
Jordi <oriondark69@gmail.com>
2020-03-05 17:57:58 +0100

    Updated version

---
+++ Assets/SavePoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+    public Canvas popUpCanvas;
+
+    public float radius;
+
+
+    private void Start()
+    {
+        popUpCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] players = Physics.OverlapSphere(transform.position, radius);
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].gameObject.tag == "Jammo")
+            {
+                popUpCanvas.gameObject.SetActive(true);
+                print(players[i].gameObject.name);
+            }
+        }
+    }
+
+
+}
